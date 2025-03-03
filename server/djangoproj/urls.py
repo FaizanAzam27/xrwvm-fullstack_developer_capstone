e77983ce1892ae1f568f7ead802e69b9918cdec1
faizanazam27 <faizanazam27@gmail.com>
2025-03-03 06:09:03 -0500

    Linting issues

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -13,14 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-import os
+
 
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path, re_path
+from django.urls import include, path
 from django.views.generic import TemplateView
-from django.views.static import serve
 
 urlpatterns = [
     path('postreview/<int:dealer_id>',
@@ -36,4 +35,3 @@ urlpatterns = [
     path('djangoapp/', include('djangoapp.urls')),
     path('', TemplateView.as_view(template_name="Home.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
